f1c3cee905c27044c72ef7bdb8734571b292d6bd
Cortecci00 <cortecci00@gmail.com>
2024-08-04 17:59:03 -0300

    Creando Login

--- ChopperStoreAngularTest/Angular/Angular18/src/app/app.module.ts
+++ ChopperStoreAngularTest/Angular/Angular18/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { IconModule, IconSetService } from '@coreui/icons-angular';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductsComponent } from './products/products.component';
@@ -18,7 +21,6 @@ import { CartComponent } from './cart/cart.component';
 import { UsComponent } from './us/us.component';
 import { ContactComponent } from './contact/contact.component';
 import { RecommendationComponent } from './recommendation/recommendation.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -31,9 +33,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     CartComponent,
     UsComponent,
     ContactComponent,
-    RecommendationComponent
+    RecommendationComponent,
   ],
   imports: [
+    MatInputModule,
     MatFormFieldModule,
     IconModule,
     FlexLayoutModule,
