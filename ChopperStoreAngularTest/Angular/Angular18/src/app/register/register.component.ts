223ad59e5e960f147181cdaf0ec2f4f291bb31e8
Cortecci00 <cortecci00@gmail.com>
2024-08-05 05:45:35 -0300

    Register front end
    Register creado

--- ChopperStoreAngularTest/Angular/Angular18/src/app/register/register.component.ts
+++ ChopperStoreAngularTest/Angular/Angular18/src/app/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent {
+  hide = signal(true);
 
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = signal('');
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('Debes ingresar un valor');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('No es un email valido');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 }
